a283c772501e7c60e6149428d9e424b69ffde6f6
pagr0009 <110451014+pagr0009@users.noreply.github.com>
2022-09-17 19:48:34 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -11,12 +11,18 @@ import numpy as np
 import flask
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
-from pickle5 import pickle
+import pickle
+from tensorflow.python.keras.backend import set_session
+
 
 app = Flask(__name__)
-TOKEN = 'sl'+'.BPZXktupEgM6-6ZSgOtdLbX0KjlK9CF6RX7AMdxcZj' + '-' + 'uZZfdmmIUB9dqMGzCMan5X23nWRd1QavxGS3w0Z' + '-' +'GAawaX84ZuikTYXspTBK81ORKEsmQ08KBR5Crts0yvtbXBbBlwxxv17A'
+# api = Api(app)
 
-@app.route('/', methods = ['PUT'])
+TOKEN = 'sl' + '.' + 'BPfGb_HgBBmL2llvmRq4HydPMp2evDVrbhqKKq'+'Vhw2LBX2lUBTqyWByc'+'ATPQIwCvV64UhhyVJqxmz2'+'SQRv2ee3bCKVyaG5GyuIDyM'+'3V9rWkHwU7z5TBPYW5'+'YcczrZpgyuZ5LY01pJR0'
+
+
+
+@app.route('/pred', methods = ['PUT'])
 def specie_pred():
     
     # Establish connection
@@ -41,13 +47,13 @@ def specie_pred():
         try:
             filenames = []
             folder_path = "/Audio Submit Form"
-
+            print(folder_path)
             # dbx object contains all functions that 
             # are required to perform actions with dropbox
             files = dbx.files_list_folder(folder_path).entries
 
             for file in files:
-
+                
                 # listing
                 filenames.append(file.name)
             return filenames
@@ -56,8 +62,11 @@ def specie_pred():
             print(str(e))
     
     def features_extractor(file_name):
+        print('here')
         audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
+        print('here2')
         mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+        print('here3')
         mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
     
         return mfccs_scaled_features
@@ -81,11 +90,12 @@ def specie_pred():
     
     dbx = connect_to_dropbox()
     filenames = list_files_in_folder()
-
+    print(filenames)
     wav_files = [i for i in filenames if i.endswith('.wav')]
     file_num = [int(i.split(" ")[0]) for i in filenames if i.endswith('.wav')]
     curr_file = max(file_num)
     user_file = [i for i in wav_files if str(curr_file) in i]
+    # print(user_file)
     mfccs_scaled_features = read_extract_from_file()
     mfccs_scaled_features = mfccs_scaled_features.reshape(1,-1)
     model = pickle.load(open("model_saved.sav",'rb'))
@@ -102,6 +112,10 @@ def specie_pred():
         pred = 'White Napped'
     #print(pred)
     return {'class': pred}
+
+@app.route('/', methods = ['GET'])
+def trial():
+    return {"home": "ok"}
 
 
 if __name__ == "__main__":
