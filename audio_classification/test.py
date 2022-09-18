785dc1dad44337561b8b40cdb3fb039320d1fbc7
pagr0009 <110451014+pagr0009@users.noreply.github.com>
2022-09-18 10:03:54 +1000

    iteration 2 final commit

--- audio_classification/test.py
+++ audio_classification/test.py
@@ -1,7 +1,9 @@
 import requests 
 import json
 
-BASE = "http://90e9-121-200-5-152.ngrok.io" 
+BASE = "http://c477-121-200-5-152.ngrok.io/pred" 
+# BASE = "http://52.63.68.159:5000"
+# BASE = "https://audio-prediction.herokuapp.com/"
 
-response = requests.put(BASE, {'link' : 'C:\\Users\\pragya\\Downloads\\Iteration 2\\test\\new_holland_honeyeater_test_1.wav'})
+response = requests.put(BASE)
 print(response.json())
