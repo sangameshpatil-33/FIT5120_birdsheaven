81f6db2e7a72bfc1b5398298173e13a785f7c04e
pagr0009 <110451014+pagr0009@users.noreply.github.com>
2022-10-09 03:52:16 +1100

    ITERATION 3 final commit

--- audio_classification/app_IT3.py
+++ audio_classification/app_IT3.py
@@ -1,6 +1,7 @@
 ## REFERENCE: https://www.geeksforgeeks.org/how-to-automate-the-storage-using-dropbox-api-in-python/
 ## REFERENCE: https://villoro.com/post/dropbox_python
 
+# import all required files
 import io
 import pathlib
 import pandas as pd
@@ -11,7 +12,6 @@ import numpy as np
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import pickle
-# import pandas.util.testing as tm
 from pyngrok import ngrok
 from pyngrok import ngrok, conf, installer
 import os
@@ -20,27 +20,33 @@ from tensorflow.python.keras.backend import set_session
 from flask_cors import CORS
 import tensorflow as tf
 import tensorflow.keras.models
-# from dotenv import load_dotenv
 
-
+# define app name for the api, call FLASK function
 app = Flask(__name__)
+# add cors to the add to connect to any platform without error
 CORS(app)
+
+# define dropbox api token
 TOKEN = '****'
 
-
+# set route to the prediction function
 @app.route('/pred', methods = ['GET'])
+# define prediction function
 def specie_pred():
     
-    # Establish connection
+    # Establish connection to dropbox
     def connect_to_dropbox():
 
         try:
+            # connect to dropbox
             dbx = dropbox.Dropbox(TOKEN)
-            print('Connected to Dropbox successfully')
-
+            
+        # if error occured,
         except Exception as e:
+            # print the error
             print(str(e))
-
+            
+        # return dropbox connection
         return dbx
     
     # explicit function to list files
@@ -51,32 +57,38 @@ def specie_pred():
         dbx = connect_to_dropbox()
 
         try:
+            # create emmpty list to store filenames
             filenames = []
+            # define folder path where files are stored in dropbox
             folder_path = "/Audio Submit Form"
-            print(folder_path)
-            # dbx object contains all functions that 
-            # are required to perform actions with dropbox
+            # using the dropbox xonnection, list all the files in the folder
             files = dbx.files_list_folder(folder_path).entries
 
+            # iterate through each file in the folder
             for file in files:
-                
-                # listing
+                # fetch the names of the file and store it in the list
                 filenames.append(file.name)
+            # return list of all file names
             return filenames
-
+        
+        # if any error occurs,
         except Exception as e:
+            # print error
             print(str(e))
     
+    # define the function to extract features
     def features_extractor(file_name):
-        print('here')
+        # load and read the data and sample rate of the audio file
         audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
-        print('here2')
+        # using the audio data and its sample rate, extract features
         mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
-        print('here3')
+        # transpose the features extracted matrix and find its mean
         mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
-    
+        
+        # return features extracted for the file name provided
         return mfccs_scaled_features
     
+    # define function to fetch file from the dropbox
     def read_extract_from_file():
     
         # here dbx is an object which is obtained
@@ -84,52 +96,83 @@ def specie_pred():
         dbx = connect_to_dropbox()
 
         try:
+            # download the required audio file
             _, res = dbx.files_download("/Audio Submit Form/"+ user_file[0])
-
+            # iterate through the content of the audio file
             with io.BytesIO(res.content) as stream:
+                # call feature extraction function for the data loaded
                 data=features_extractor(stream)
+            # return the extracted feature data
             return data
-
+        # if error occurs,
         except Exception as e:
+            # print errors
             print(str(e))
     
-    
+    # call dropbox connection function build a connection
     dbx = connect_to_dropbox()
+    # call the list files function to fetch all filenames from the dropbox folder
     filenames = list_files_in_folder()
-    print(filenames)
+    # from the fetched filenames, find all files in "wav" format
     wav_files = [i for i in filenames if i.endswith('.wav')]
+    # to find the latest file submitted to the dropbox,
+    # extract only the starting numerical bits of each filename and convert it into an integer format
     file_num = [int(i.split(" ")[0]) for i in filenames if i.endswith('.wav')]
+    # find the max value from the list to state the latest file
     curr_file = max(file_num)
+    # check the substring of the latest file in the list containing names of all wav files
     user_file = [i for i in wav_files if str(curr_file) in i]
-    print(user_file)
+    # call the extract file function to read the data and extract features
     mfccs_scaled_features = read_extract_from_file()
+    # reshape the features in the required format
     mfccs_scaled_features = mfccs_scaled_features.reshape(1,-1)
+    # load the best saved model
     model = tf.keras.models.load_model('saved_models/weights.best.sequential.hdf5')
+    # using the loaded trained model, predict label for the new audio file based on features
     predicted_label=model.predict(mfccs_scaled_features)
+    # find the class with maximum probability 
     classes_x=np.argmax(predicted_label,axis=1)
     
+    # as per the label encoder in the 3_model_training_save_IT3 file, transform the encoded label to its original format
+    
+    # check if the class is 0
     if classes_x == [0]:
+        # if true, set the category 
         pred = 'Brown Headed Honeyeater'
+    # check if the class is 1
     elif classes_x == [1]:
+        # if true, set the category 
         pred = 'Melbourne Skyline Bird'
+    # check if the class is 2
     elif classes_x == [2]:
+        # if true, set the category 
         pred = 'New Holland Honeyeater'
+    # check if the class is 3
     elif classes_x == [3]:
+        # if true, set the category 
         pred = 'Not Bird'
+    # check if the class is 4
     elif classes_x == [4]:
+        # if true, set the category 
         pred = 'White Napped Honeyeater'
     else:
         pass
     
-    #print(pred)
+    # return the predicted category in json format
     return {'SPECIE': pred}
 
+# Trial function is built to check if the server connection is built.
+# it is one of the debugging options. In case the prediction function does not work, 
+# we can call trial function to check if the connection wsa successfull
+# set route to the trial function
 @app.route('/', methods = ['GET'])
 def trial():
+    # return json value home ok
     return {"home": "ok"}
 
-
+# call the main class
 if __name__ == "__main__":
+    # set configuration to convert the local machine as the dedicated server
     pyngrok_config = conf.get_default()
     if not os.path.exists(pyngrok_config.ngrok_path):
         myssl = ssl.create_default_context();
@@ -137,7 +180,10 @@ if __name__ == "__main__":
         myssl.verify_mode=ssl.CERT_NONE
         installer.install_ngrok(pyngrok_config.ngrok_path, context=myssl)
 
+    # build a connection to port 8000
     ngrok_tunnel = ngrok.connect(8000)
+    # create public url 
     print("PUBLIC URL:", ngrok_tunnel.public_url)
     nest_asyncio.apply()
+    # run app on port 8000 and with the parameter debug as True
     app.run(debug = True, port = 8000)
--- audio_classification/sound_record_api.py
+++ audio_classification/sound_record_api.py
@@ -1,6 +1,7 @@
 ## REFERENCE: https://www.geeksforgeeks.org/how-to-automate-the-storage-using-dropbox-api-in-python/
 ## REFERENCE: https://villoro.com/post/dropbox_python
 
+# import required libraries
 import io
 import pathlib
 import pandas as pd
@@ -19,25 +20,31 @@ from flask_cors import CORS
 import pyaudio
 import wave
 
-
+# define app name for the api, call FLASK function
 app = Flask(__name__)
+# add cors to the add to connect to any platform without error
 CORS(app)
+
+# define dropbox api token
 TOKEN = '****'
 
-
+# set route to the record function
 @app.route('/record', methods = ['GET'])
 def specie_pred():
     
-    # Establish connection
+    # Establish connection to dropbox
     def connect_to_dropbox():
 
         try:
+            # connect to dropbox
             dbx = dropbox.Dropbox(TOKEN)
-            print('Connected to Dropbox successfully')
-
+            
+        # if error occured,
         except Exception as e:
+            # print the error
             print(str(e))
-
+            
+        # return dropbox connection
         return dbx
     
     # explicit function to list files
@@ -48,41 +55,53 @@ def specie_pred():
         dbx = connect_to_dropbox()
 
         try:
+            # create emmpty list to store filenames
             filenames = []
+            # define folder path where files are stored in dropbox
             folder_path = "/Audio Submit Form"
-            print(folder_path)
-            # dbx object contains all functions that 
-            # are required to perform actions with dropbox
+            # using the dropbox xonnection, list all the files in the folder
             files = dbx.files_list_folder(folder_path).entries
 
+            # iterate through each file in the folder
             for file in files:
-                
-                # listing
+                # fetch the names of the file and store it in the list
                 filenames.append(file.name)
+            # return list of all file names
             return filenames
-
+        
+        # if any error occurs,
         except Exception as e:
+            # print error
             print(str(e))
             
+    # define audio recording function
     def record_audio(filename):
+        # set filename to save the audio data
         filename = filename + ' -user.wav'
-        chunk = 1024  # Record in chunks of 1024 samples
-        sample_format = pyaudio.paInt16  # 16 bits per sample
+        # Record in chunks of 1024 samples
+        chunk = 1024  
+        # 16 bits per sample
+        sample_format = pyaudio.paInt16  
         channels = 2
-        fs = 44100  # Record at 44100 samples per second
-        seconds = 5
+        # Record at 44100 samples per second
+        fs = 44100  
+        # time limit set for the recording
+        seconds = 20
+        
+        # Create an interface to PortAudio
+        p = pyaudio.PyAudio()  
+        
+#         print('Recording')
 
-        p = pyaudio.PyAudio()  # Create an interface to PortAudio
-
-        print('Recording')
-
+        # listen to audio data with the set paramters
         stream = p.open(format=sample_format,
                         channels=channels,
                         rate=fs,
                         frames_per_buffer=chunk,
                         input=True)
-
-        frames = []  # Initialize array to store frames
+        
+         # Initialize array to store frames
+        frames = [] 
 
         # Store data in chunks for 3 seconds
         for i in range(0, int(fs / chunk * seconds)):
@@ -113,25 +132,37 @@ def specie_pred():
         with open(file_from, 'rb') as f:
             dbx.files_upload(f.read(), file_to)
     
+    # call dropbox connection function build a connection
     dbx = connect_to_dropbox()
+    # call the list files function to fetch all filenames from the dropbox folder
     filenames = list_files_in_folder()
+    # from the fetched filenames, find all files in "wav" format
     wav_files = [i for i in filenames if i.endswith('.wav')]
+    # to find the latest file submitted to the dropbox,
+    # extract only the starting numerical bits of each filename and convert it into an integer format
     file_num = [int(i.split(" ")[0]) for i in wav_files]
+    # find the max value from the list to state the latest file and add 1 to set it as the latest file being added
     filename = max(file_num) + 1
-    print(filename)
+    # call record function 
     record_audio(str(filename))
+    # call the send audio function to send the recorded data to dropbox
     send_audio(str(filename))
     
-    
-    
+    # return a message for the user in json format
     return {'RESULT': 'Your audio has been recorded. Proceed to Predict Button to know your backyard bird.'}
 
+# Trial function is built to check if the server connection is built.
+# it is one of the debugging options. In case the record function does not work, 
+# we can call trial function to check if the connection wsa successfull
+# set route to the trial function
 @app.route('/', methods = ['GET'])
 def trial():
+    # return json value home ok
     return {"home": "ok"}
 
-
+# call the main class
 if __name__ == "__main__":
+    # set configuration to convert the local machine as the dedicated server
     pyngrok_config = conf.get_default()
     if not os.path.exists(pyngrok_config.ngrok_path):
         myssl = ssl.create_default_context();
@@ -139,7 +170,10 @@ if __name__ == "__main__":
         myssl.verify_mode=ssl.CERT_NONE
         installer.install_ngrok(pyngrok_config.ngrok_path, context=myssl)
 
+    # build a connection to port 8000
     ngrok_tunnel = ngrok.connect(8000)
+    # create public url 
     print("PUBLIC URL:", ngrok_tunnel.public_url)
     nest_asyncio.apply()
+    # run app on port 8000 and with the parameter debug as True
     app.run(debug = True, port = 8000)
